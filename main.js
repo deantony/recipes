ff4325fee95a90f918597e77b9c96b8e0c8c6911
Tony <52037922+deantony@users.noreply.github.com>
2020-06-17 00:19:41 -0600

    Update main.js
    inset shadow(toggled)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = function(){
    let user_url = window.location.href;
 
     if(user_url == "https://deantony.github.io/recipes/index.html" || user_url == "https://deantony.github.io/recipes/"){
-        nav_home.style.boxShadow = "none"
+        nav_home.style.boxShadow = "inset 0 6px 8px 0 rgba(0, 0, 0, .2)"
         nav_home.onclick = function(){
             return false; 
         }
