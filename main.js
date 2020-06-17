af3b4b2faafb25737a99324feb3a386a2522a2ee
Tony <52037922+deantony@users.noreply.github.com>
2020-06-16 23:45:05 -0600

    disable nav click on active

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-
+//Disable right click on cards
 let cards = document.getElementsByClassName("card");
 for(var i = 0; i < cards.length; i++){
     var card = cards[i];
     card.oncontextmenu = function(){
         return false;
+    }
+}
+//read url and disable corresponding nav link
+nav_home = document.getElementById("nav-home");
+window.onload = function(){
+   let user_url = window.location.href;
+    if(user_url == "https://deantony.github.io/recipes/index.html" || user_url == "https://deantony.github.io/recipes/"){
+        nav_home.cursor = "not-allowed"
     }
 }
