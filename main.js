dd5c66575da7ee269e35154b31d71ccf9736b51d
Tony <52037922+deantony@users.noreply.github.com>
2020-06-16 23:55:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ nav_home = document.getElementById("nav-home");
 window.onload = function(){
    let user_url = window.location.href;
    alert(`Your URL is ${user_url}`)
+   function func_nav_home_enter(){
+       nav_home.cursor = "not-allowed";
+   }
     if(user_url == "https://deantony.github.io/recipes/index.html" || user_url == "https://deantony.github.io/recipes/"){
         nav_home.onmouseenter = function(){
-            nav_home.cursor = "not-allowed"
-            console.log("True")
+            nav_home.onmouseenter = func_nav_home_enter;
         }
     }
 }
